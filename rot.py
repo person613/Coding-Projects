91066f0f6594619079a951029d5ec61d8a1f689e
person613 <henrylhye@gmail.com>
2024-12-07 20:51:26 -0500

    Add files via upload
    Basic python programs for stuff

---
+++ rot.py
@@ -0,0 +1,13 @@
+def rot(message,rotby):
+    letterindex = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    input = message
+    output = ""
+    for i in range(len(input)):
+        for x in range(26):
+            if input[i] == letterindex[x]:
+                output = output + letterindex[(x+rotby)%26]
+        if input[i] == " ":
+            output = output + " "
+    return output
+    
+print(rot(str(input("What do you want to rotate? (Capital Only :\)")),int(input("How much do you want to rotate by?"))))
